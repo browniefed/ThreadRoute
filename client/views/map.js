1f26a315494889e26a593fd87a95ad3ab6cd5b26
Jason Brown <iam@browniefed.com>
2014-02-22 22:13:47 -0800

    Stuff happened

--- client/views/map.js
+++ client/views/map.js
@@ -9,28 +9,5 @@ Template.map.rendered = function() {
 	map.setView(new L.LatLng(45.525292, -122.668197),13);
 	map.addLayer(osm);
 
-	var groupedPolylines = {}, tempPolyline;
-
-	_(polylines).each(function(route, key) {
-		groupedPolylines[key] = groupedPolylines[key] || {};
-
-		_(route).each(function(directionRoute, direction) {
-
-			var multiPolyLineStore = [];
-			groupedPolylines[key][direction] = groupedPolylines[key][direction] || [];
-
-			if (directionRoute && directionRoute.length) {
-	            _(directionRoute).each(function(line) {
-	            	multiPolyLineStore.push(decodeLine(line));
-	            });
-	            tempPolyline = L.multiPolyline(multiPolyLineStore, {color: '#2666e9', opacity: .3, className: 'route-' + key + '-' + direction});
-	            tempPolyline.addTo(map);
-				groupedPolylines[key][direction].push(tempPolyline);
-
-        	}
-
-		})
-       
-    });
 
 }
